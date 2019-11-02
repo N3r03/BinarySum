be50037caf03171f1314362a3d9f81ed577771bb
nerminajakupovska@gmail.com <nerminajakupovska@gmail.com>
2019-11-02 05:11:43 +0100

    Add project files.

---
+++ BinarySum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BinarySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, sum;
+            Console.WriteLine("Enter two numbers to get the sum in binary form.");
+            Console.WriteLine("First number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            sum = num1 + num2;
+           // Console.WriteLine(sum);
+            string binary = Convert.ToString(sum,2);
+            Console.WriteLine($"Binary sum: {binary}" );
+        }
+    }
+}
